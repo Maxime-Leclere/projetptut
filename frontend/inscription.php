ef350bdc03783e1539cddc4823c7b378c087271b
l17002876 <maxleclere1999@gmail.com>
2019-12-19 14:22:23 +0100

    creer inscription

---
+++ frontend/inscription.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php include_once 'head.php' ?>
+</head>
+
+<body>
+<?php include_once 'header.php' ?>
+
+<main class="custom_body">
+    <div class="content_inscription">
+        <label id="label_email">E-mail</label>
+        <input id="email" required>
+        <label id="label_password">Mot de passe</label>
+        <input id="password" required>
+        <button id="button_inscription" type="submit">se connecter</button>
+    </div>
+</main>
+
+<?php include_once 'footer.php' ?>
+</body>
+</html>
+
