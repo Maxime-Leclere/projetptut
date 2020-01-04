3cd33eb3038845822e59740feded41866ac0818e
Maxime-Leclere <maxime.leclere@etu.univ-amu.fr>
2020-01-04 16:38:41 +0100

    modif de Date

--- backend/Date.php
+++ backend/Date.php
@@ -22,7 +22,8 @@ class Date {
             $d = date('j', intval($date));
             $w = str_replace('0', '7', $date->format('w'));
             $tab[$y][$m][$d] = $w;
-            $date->add(new DateInterval('P1D'));
+//            $date->add(new DateInterval('P1D'));
+            date_add($date, date_interval_create_from_date_string('1 day'));
         }
         return $tab;
     }
