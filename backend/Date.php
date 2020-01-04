dba9f6d686e54147211a6f8076aa799774e6dc03
Maxime-Leclere <maxime.leclere@etu.univ-amu.fr>
2020-01-04 16:54:37 +0100

    modif de Date

--- backend/Date.php
+++ backend/Date.php
@@ -17,14 +17,21 @@ class Date {
             echo 'error initialisize DateTime';
         }
         while ($date->format('Y') <= $year) {
-            $y = date('Y', intval($date));
-            $m = date('n', intval($date));
-            $d = date('j', intval($date));
+            $y = $date->format('Y');
+            $m = $date->format('n');
+            $d = $date->format('j');
             $w = str_replace('0', '7', $date->format('w'));
             $tab[$y][$m][$d] = $w;
 //            $date->add(new DateInterval('P1D'));
             date_add($date, date_interval_create_from_date_string('01 days'));
         }
+//        $date = strtotime($year.'-01-01');
+//        while ($date->format('Y') <= $year) {
+//            $y = date('', $date);
+//            $m = date('', $date);
+//            $d = date('', $date);
+//            $w = date('', $date);
+//        }
         return $tab;
     }
 }
