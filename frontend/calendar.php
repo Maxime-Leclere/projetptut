102a822ef266f63d0551fd96f95b05715bbc2af9
Maxime-Leclere <maxime.leclere@etu.univ-amu.fr>
2020-01-04 17:42:18 +0100

    ajout jquery

--- frontend/calendar.php
+++ frontend/calendar.php
@@ -4,9 +4,10 @@
     <?php include_once 'head.php' ?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
     <script type="text/javascript">
-        jQuery(function ($)){
-            $('.month').hide()
-        }
+        jQuery(function ($){
+            $('.daysofmonth').hide();
+            $('.daysofmonth:first').show();
+        });
     </script>
 </head>
 
