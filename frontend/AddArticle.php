2914b3db7d8eb8fe21d5a85f5a0c0b7d5b1e8bcd
Maxime-Leclere <maxime.leclere@etu.univ-amu.fr>
2020-01-06 00:04:57 +0100

    modif requete

--- frontend/AddArticle.php
+++ frontend/AddArticle.php
@@ -1,7 +1,7 @@
 <?php
     global $DB;
 
-    $req = $DB->query('INSERT INTO ARTICLES(Title_A, Text_A) VALUES (\''.$_POST['Title'].'\', \''.$_POST['Text'].'\')');
+    $req = $DB->query('INSERT INTO ARTICLES(Title_A, Text_A) VALUES (\''.$_POST['title'].'\', \''.$_POST['text'].'\')');
     $req->execute();
     header('Location: home.php');
 ?>
