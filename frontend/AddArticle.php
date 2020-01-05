06d9240f43ae169504ed0872b62797678c183a29
Maxime-Leclere <maxime.leclere@etu.univ-amu.fr>
2020-01-06 00:25:39 +0100

    modif requete

--- frontend/AddArticle.php
+++ frontend/AddArticle.php
@@ -2,7 +2,7 @@
     global $DB;
     $title = $_POST['title'];
     $text = $_POST['text'];
-    $req = $DB->query("INSERT INTO `ARTICLES`(`Title_A`, `Text_A`) VALUES ('$title', '$text')");
+    $req = $GLOBALS['basededonne']->query("INSERT INTO `ARTICLES`(`Title_A`, `Text_A`) VALUES ('$title', '$text')");
     $req->execute();
     header('Location: home.php');
 ?>
--- frontend/config.php
+++ frontend/config.php
@@ -2,6 +2,7 @@
     try {
         $DB = new PDO('mysql:host=mysql-projetptut.alwaysdata.net;dbname=projetptut_database;',
             '195907', 'Azertyuiop1999');
+        $GLOBALS['basededonne'] = $DB;
     } catch (PDOException $e) {
         echo 'base de donner totalement bugger';
         exit();
