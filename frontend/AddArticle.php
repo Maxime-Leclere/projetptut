8d0ab32168220ba75c79edcfaef90e3b581b0371
l17002876 <maxleclere1999@gmail.com>
2020-01-06 12:30:44 +0100

    include

--- frontend/AddArticle.php
+++ frontend/AddArticle.php
@@ -1,8 +1,9 @@
 <?php
+    include_once 'config.php';
     global $DB;
     $title = $_POST['title'];
     $text = $_POST['text'];
-    $req = $GLOBALS['basededonne']->query("INSERT INTO `ARTICLES`(`Title_A`, `Text_A`) VALUES ('$title', '$text')");
+    $req = $DB->query("INSERT INTO `ARTICLES`(`Title_A`, `Text_A`) VALUES ('$title', '$text')");
     $req->execute();
     header('Location: home.php');
 ?>
