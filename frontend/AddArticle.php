a60c69a4eb1e66b8a697dd05ae7483a71572b757
l17002876 <maxleclere1999@gmail.com>
2020-01-06 12:33:21 +0100

    include

--- frontend/AddArticle.php
+++ frontend/AddArticle.php
@@ -4,6 +4,6 @@
     $title = $_POST['title'];
     $text = $_POST['text'];
     $req = $DB->query("INSERT INTO `ARTICLES`(`Title_A`, `Text_A`) VALUES ('$title', '$text')");
-    $req->execute();
+//    $req->execute();//
     header('Location: home.php');
 ?>
