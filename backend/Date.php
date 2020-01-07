65a2055a67e6dc8861745346beb584c922116833
l17002876 <maxleclere1999@gmail.com>
2020-01-07 15:09:41 +0100

    ajout image dans bd

--- backend/Date.php
+++ backend/Date.php
@@ -17,8 +17,9 @@ class Date {
                         ORDER BY Date_A DESC, Time_A DESC');
         $i = 0;
         while ($d = $req->fetch(\PDO::FETCH_OBJ)) {
+            header("Content-Type: image");
             $r[$i] = '<h1>'.$d->Title_A.'</h1><p>'.$d->Date_A.' '.
-            $d->Time_A.'</p><img src="data:image/jpeg;base64{'.$d->Image_A.'}"/><p>'.$d->Text_A.'</p>';
+            $d->Time_A.'</p>'.$d->Image_A.'<p>'.$d->Text_A.'</p>';
             ++$i;
         }
         return $r;
