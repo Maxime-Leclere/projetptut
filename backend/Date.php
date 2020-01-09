0d193d3e826a94366149310cb32526b8915ef518
l17002876 <maxleclere1999@gmail.com>
2020-01-09 15:13:42 +0100

    modif css

--- backend/Date.php
+++ backend/Date.php
@@ -67,8 +67,9 @@ class Date {
             ++$i;
         }
         $reqM = $DB->query("SELECT M.Num_M, Date_M, Heure, Club_Adversaire, M.Lieu, 
-        Nom_Equipe FROM MATCHS M, EQUIPE E, Jouer J WHERE M.Num_M = J.Num_M AND 
-        J.Num_Equipe = E.Num_Equipe AND M.Lieu='Aix-en-Provence' AND YEAR(Date_M) = $year ORDER BY Date_M DESC, Heure DESC");
+        Nom_Equipe FROM MATCHS M, EQUIPE2 E, Jouer J WHERE M.Num_M = J.Num_M AND 
+        J.Num_Equipe = E.Num_Equipe AND M.Lieu='Aix-en-Provence' AND Date_M >= $dateCurrent AND Heure >= '".$timeCurrent.
+            "' ORDER BY Date_M DESC, Heure DESC");
         while($d2 = $reqM->fetch(\PDO::FETCH_OBJ)) {
             $r[$i] = '<h3>'.$d2->Lieu.' contre '. $d2->Club_Adversaire.'</h3><p>'.$d2->Date_M.
                 ' '.$d2->Heure.'</p>';
