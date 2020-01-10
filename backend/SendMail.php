948e1a7191b080b71032e8d785b64a72e7083e7b
l17002876 <maxleclere1999@gmail.com>
2020-01-10 17:11:02 +0100

    contact envoyer mail

--- backend/SendMail.php
+++ backend/SendMail.php
@@ -2,7 +2,9 @@
     $mail = $_POST['email'];
     $subject = $_POST['sujet'];
     $message = $_POST['message'];
+var_dump($mail);
+var_dump($subject);
+var_dump($message);
 
-
-    mail($mail, $subject, $message);
-    header('Location: ../frontend/contact.php');
+    mail($mail, addslashes($subject), addslashes($message));
+//    header('Location: ../frontend/contact.php');
