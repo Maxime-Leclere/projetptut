3dfce35931b681e974f0acc0e2452bff54af2a4a
l17002876 <maxleclere1999@gmail.com>
2020-01-10 17:14:16 +0100

    contact envoyer mail

--- backend/SendMail.php
+++ backend/SendMail.php
@@ -2,9 +2,6 @@
     $mail = $_POST['email'];
     $subject = $_POST['sujet'];
     $message = $_POST['message'];
-var_dump($mail);
-var_dump($subject);
-var_dump($message);
 
     mail($mail, addslashes($subject), addslashes($message));
-//    header('Location: ../frontend/contact.php');
+    header('Location: ../frontend/contact.php');
