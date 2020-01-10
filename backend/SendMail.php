582ac47fe0c98aaeef1be9ebfe237c2f4d3f153b
l17002876 <maxleclere1999@gmail.com>
2020-01-10 16:55:18 +0100

    contact envoyer mail

---
+++ backend/SendMail.php
@@ -0,0 +1,8 @@
+<?php
+    $mail = $_POST['email'];
+    $subject = $_POST['sujet'];
+    $message = $_POST['message'];
+
+
+    mail($mail, $subject, $message);
+    header('Location: contact.php');
--- frontend/contact.php
+++ frontend/contact.php
@@ -18,6 +18,26 @@
             <textarea name="message" required></textarea>
             <input name="submit">
         </form>
+
+<!--        <div class="container" style="height: 100%"><br>-->
+            <form action="backend/SendMail.php" method="post">
+<!--                <div class="form-group">-->
+                    <label for="email">Email addresse:</label>
+                    <input type="email" class="form-control" placeholder="Enter email" id="email" name="Email">
+<!--                </div>-->
+<!--                <div class="form-group">-->
+                    <label for="sujet">Sujet:</label>
+                    <input type="text" class="form-control" id="sujet">
+<!--                </div>-->
+<!--                <div class="form-group">-->
+                    <label for="comment">Message:</label>
+                    <textarea class="form-control" rows="5" id="message"></textarea>
+<!--                </div>-->
+
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+<!--        </div>-->
+
     </main>
 
     <?php include_once 'footer.php' ?>
