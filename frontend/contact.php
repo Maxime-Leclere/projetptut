2f20229c2ec56adb85c74f75d66baca8e06e46b8
Maxime-Leclere <maxime.leclere@etu.univ-amu.fr>
2020-01-11 17:29:31 +0100

    ajouter partie flo : nospartenaires boutique terminer

--- frontend/contact.php
+++ frontend/contact.php
@@ -5,41 +5,30 @@
     </head>
 
     <body>
-    <?php include_once 'header.php' ?>
+        <?php include_once 'header.php' ?>
 
-    <main class="custom_body">
-        <h1>Formulaire de contact</h1>
-        <form>
-            <label>E-mail</label>
-            <input name="email" required>
-            <label>Sujet</label>
-            <input name="sujet" required>
-            <label>Message</label>
-            <textarea name="message" required></textarea>
-            <input name="submit">
-        </form>
-
-<!--        <div class="container" style="height: 100%"><br>-->
+        <div class="container" style="height: 100%"><br>
             <form action="../backend/SendMail.php" method="post">
-<!--                <div class="form-group">-->
+                <h1>Formulaire de contact</h1>
+                <div class="form-group">
                     <label for="email">Email addresse:</label>
                     <input type="email" class="form-control" placeholder="Enter email" name="email">
-<!--                </div>-->
-<!--                <div class="form-group">-->
+                </div>
+
+                <div class="form-group">
                     <label for="sujet">Sujet:</label>
                     <input type="text" class="form-control" name="sujet">
-<!--                </div>-->
-<!--                <div class="form-group">-->
+                </div>
+
+                <div class="form-group">
                     <label for="comment">Message:</label>
                     <textarea class="form-control" rows="5" name="message"></textarea>
-<!--                </div>-->
-
+                </div>
+                
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
-<!--        </div>-->
+        </div>
 
-    </main>
-
-    <?php include_once 'footer.php' ?>
+        <?php include_once 'footer.php' ?>
     </body>
 </html>
