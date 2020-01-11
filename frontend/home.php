cb80fce2703723ed196ca1b5198e09c589e225ba
Maxime-Leclere <maxime.leclere@etu.univ-amu.fr>
2020-01-11 13:15:18 +0100

    un peu de rangement

--- frontend/home.php
+++ frontend/home.php
@@ -10,7 +10,7 @@
         <main class="custom_body">
             <?php
             include_once '../backend/Date.php';
-            require ("../backend/Config.php");
+            require ("/../backend/Config.php");
             use backend\Date;
             $date   = new Date();
             $year   = \date('Y');
