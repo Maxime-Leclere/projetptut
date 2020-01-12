b5848a41bda0a321e50e46a5b871ac4caab6d97d
Maxime-Leclere <maxime.leclere@etu.univ-amu.fr>
2020-01-12 14:57:15 +0100

    ajouter page admin

--- frontend/adminEvent.php
+++ frontend/adminEvent.php
@@ -23,36 +23,38 @@
         <div class="container">
             <table>
                 <thead>
-                <tr>
-                    <?php foreach ($date->attrTournoi as $attr) {?>
-                        <th><?php echo '<p class="name_attr_tournoi">'.$attr.'</p>' ?></th>
-                    <?php } ?>
-                </tr>
+                    <tr>
+                        <?php foreach ($date->attrTournoi as $attr) {?>
+                            <th><?php echo '<p class="name_attr_tournoi">'.$attr.'</p>' ?></th>
+                        <?php } ?>
+                    </tr>
                 </thead>
                 <tbody>
-                <?php foreach ($tournoi as $t) {?>
-                    <tr>
+                    <?php foreach ($tournoi as $t) {?>
+                        <tr>
                             <?php $attr_tournoi = $t->getAttr();?>
                             <?php foreach ($attr_tournoi as $attrT) {?>
-                                <td><?php echo '<p class="attr_tournoi">'.$attrT.'</p>'?></td>
+                                <td>
+                                    <?php echo '<p class="attr_tournoi">'.$attrT.'</p>'?>
+                                </td>
                             <?php } ?>
-                    </tr>
-                <?php }?>
+                        </tr>
+                    <?php }?>
                 </tbody>
             </table>
 
             <table>
                 <thead>
-                <tr>
-                    <?php foreach ($date->attrMatch as $attr) {?>
-                        <th><?php echo '<p class="name_attr_match">'.$attr.'</p>' ?></th>
-                    <?php } ?>
-                </tr>
+                    <tr>
+                        <?php foreach ($date->attrMatch as $attr) {?>
+                            <th><?php echo '<p class="name_attr_match">'.$attr.'</p>' ?></th>
+                        <?php } ?>
+                    </tr>
                 </thead>
                 <tbody>
                 <?php foreach ($match as $m) {?>
                     <tr>
-                        <?php$attr_match = $m->getAttr();?>
+                        <?php $attr_match = $m->getAttr();?>
                         <?php foreach ($attr_match as $attrM) {?>
                             <td>
                                 <?php echo '<p class="attr_match">'.$attrM.'</p>'?>
