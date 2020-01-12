bd767c45b30c6de5d407cd748000ec1fe68e1362
Maxime-Leclere <maxime.leclere@etu.univ-amu.fr>
2020-01-12 17:02:21 +0100

    test var de form

--- backend/AddTournoi.php
+++ backend/AddTournoi.php
@@ -1,14 +1,15 @@
 <?php
-    $name    = addslashes($_POST['name']);
+    $name    = $_POST['name'];
     $dateDeb = $_POST['datedeb'];
     $dateFin = $_POST['datefin'];
-    $lieu    = addslashes($_POST['lieu']);
+    $lieu    = $_POST['lieu'];
 
     var_dump($name);
     var_dump($dateDeb);
     var_dump($dateFin);
     var_dump($lieu);
     $req = $DB->query("INSERT INTO `TOURNOI`(`Nom_T`, `Date_deb`,
-    `Date_fin`, `Lieu`) VALUES ('$name', '$dateDeb', '$dateFin', '$lieu')");
+    `Date_fin`, `Lieu`) VALUES ('".addslashes($name)."', '$dateDeb', '$dateFin', 
+    '".addslashes($lieu)."')");
 
     header("Location: ../frontend/adminEvent.php");
