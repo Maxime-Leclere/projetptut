b703b3a030b20b9197bb7be3ab7e0a9e6e2f5dba
Maxime-Leclere <maxime.leclere@etu.univ-amu.fr>
2020-01-12 14:32:00 +0100

    ajouter page admin

--- backend/Date.php
+++ backend/Date.php
@@ -4,8 +4,6 @@ namespace backend;
 use DateInterval;
 use DateTime;
 use Exception;
-use backend\Tournoi;
-use backend\Match;
 
 class Date {
     var $days   = array('Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche');
--- backend/Tournoi.php
+++ backend/Tournoi.php
@@ -1,5 +1,6 @@
 <?php
 namespace backend;
+
 class Tournoi {
     private $id;
     private $nom;
