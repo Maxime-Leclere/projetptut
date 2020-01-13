5fdcbb8a32fc65a2b2c0b9f16cd11c43b3ccc30d
l17002876 <maxleclere1999@gmail.com>
2020-01-13 11:28:49 +0100

    corrige bug derniere minute

--- backend/AddMatch.php
+++ backend/AddMatch.php
@@ -2,13 +2,12 @@
     include_once 'Config.php';
 
     $date   = $_POST['date'];
-    $date   = str_replace(" 00:00:00","",$date);
     $time   = $_POST['time'].':00';
     $nameCA = $_POST['nameCA'];
     $lieu   = $_POST['lieu'];
     $numT   = $_POST['numT'];
 
     $req = $DB->query("INSERT INTO `MATCHS`(`Date_M`, `Heure`, `Club_Adversaire`, `Lieu`, 
-            `Num_T`) VALUES ('$date', '$time', '".addslashes($nameCA)."', '".addslashes($lieu)."', $numT)");
+            `Num_T`) VALUES ('".str_replace(" 00:00:00","",$date)."', '$time', '".addslashes($nameCA)."', '".addslashes($lieu)."', $numT)");
 
     header("Location: ../frontend/adminEvent.php");
