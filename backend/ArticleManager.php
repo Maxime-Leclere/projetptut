d78439eaeb981014380862872024614562700df2
l17002876 <maxleclere1999@gmail.com>
2020-02-19 16:15:06 +0100

    ajout article page

--- backend/ArticleManager.php
+++ backend/ArticleManager.php
@@ -7,7 +7,7 @@ class ArticleManager {
         global $DB;
         $req = $DB->query("SELECT Num_A, Title_A, Text_A, Date_A, Time_A, Image_A
             FROM ARTICLES WHERE Num_A = ".$id);
-        $data = $req->fetch(PDO::FETCH_OBJ);
+        $data = $req->fetch(\PDO::FETCH_OBJ);
         $articleData = new Article($data->Num_A, $data->Title_A, $data->Text_A,
             $data->Date_A, $data->Time_A, $data->Image_A);
         $articleData->setDate_A(new \DateTime($articleData->getDate_A()));
