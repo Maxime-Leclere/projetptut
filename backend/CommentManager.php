8f5737bc2483cf285acdb6b09e9f9ad491348dd0
l17002876 <maxleclere1999@gmail.com>
2020-02-20 10:47:36 +0100

    ajout commentaire

--- backend/CommentManager.php
+++ backend/CommentManager.php
@@ -4,13 +4,14 @@ namespace backend;
 class CommentManager {
     public function getFromArticle($id) {
         global $DB;
+        $r = array();
         $req = $DB->query("SELECT Num_C, Auteur_C, Contenu_C, Date_creation, Num_A
             FROM COMMENTAIRE WHERE Num_A = ".$id);
-        $data = $req->fetch(\PDO::FETCH_OBJ);
-        $articleData = new Comment(intval($data->Num_C), $data->Auteur_C,
-        $data->Contenu_C, $data->Date_creation, $data->Num_A);
-        // $articleData->setDate_A(new \DateTime($articleData->getDate_A()));
-        return $articleData;
+        while ($data = $req->fetch(\PDO::FETCH_OBJ)) {
+            $r[] = new Comment(intval($data->Num_C), $data->Auteur_C,
+                $data->Contenu_C, $data->Date_creation, $data->Num_A);
+        }
+        return $r;
     }
 
     public function add(Comment $comment) {
